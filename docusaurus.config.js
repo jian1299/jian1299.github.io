6c23f6f6e0e0d1117705294d48ac50cbbeb0b4ad
jian1299 <894504231@qq.com>
2023-07-14 14:59:02 +0800

    iu

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,16 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://jian1299.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'jian1299', // Usually your GitHub org/user name.
+  projectName: 'jian1299.github.io', // Usually your repo name.
+  deploymentBranch: 'mypages', // 部署到的分支名
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
